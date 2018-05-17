6c8a528856e6a5184fd51bc64f107d3bf53c7ecd
Simon Narduzzi <simon.narduzzi@gmail.com>
2018-05-17 18:11:21 +0200

    merged summaries

--- Unet/callbacks.py
+++ Unet/callbacks.py
@@ -131,17 +131,15 @@ class TrainValTensorBoard(TensorBoard):
             image_tensor = image_original.reshape(1, image_original.shape[0], image_original.shape[1], 1)
             lesion_tensor = lesion_original.reshape(1, lesion_original.shape[0], lesion_original.shape[1], 1)
 
-            pred_summary = tf.summary.image("prediction", pred_tensor, max_outputs=1)
-            image_summary = tf.summary.image("input", image_tensor, max_outputs=1)
-            lesion_summay = tf.summary.image("lesion", lesion_tensor,max_outputs=1)
-            merged_summary = tf.summary.image("merged", merged_image, max_outputs=1)
+            pred_summary = tf.summary.image("image/prediction", pred_tensor, max_outputs=1)
+            image_summary = tf.summary.image("image/input", image_tensor, max_outputs=1)
+            lesion_summay = tf.summary.image("image/lesion", lesion_tensor,max_outputs=1)
+            merged_summary = tf.summary.image("image/merged", merged_image, max_outputs=1)
 
+            summary_op = tf.summary.merge_all()
             # Run and add summary.
-            result = self.sess.run([pred_summary, image_summary, lesion_summay, merged_summary])
-            self.val_writer.add_summary(result[0], epoch)
-            self.val_writer.add_summary(result[1], epoch)
-            self.val_writer.add_summary(result[2], epoch)
-            self.val_writer.add_summary(result[3], epoch)
+            result = self.sess.run(summary_op)
+            self.val_writer.add_summary(result)
 
     def __add_pr_curve(self, epoch):
         if self.pr_curve and self.validation_data:
