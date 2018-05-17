c1d5ab9cd31d2277ca95ab57c0c1d1abfb0471e7
Simon Narduzzi <simon.narduzzi@gmail.com>
2018-05-17 16:58:34 +0200

    reshaping patch in predict

--- Unet/predict.py
+++ Unet/predict.py
@@ -28,12 +28,18 @@ def load_old_model(model_file):
         else:
             raise error
 
+
 def predict_patch(patch, model):
-    return model.predict(patch)
+    patch_extended = patch.reshape(1, 1, patch.shape[0], patch.shape[1], patch.shape[2])
+    return model.predict(patch_extended)[0, 0, :, :, :]
+
 
 def predict(image, model, patch_size, verbose=0):
     original_image_size = image.shape
     image_patches = create_patches_from_images(image, patch_size)
+    # Extend with channel
+    image_patches = [x.reshape(1, 32, 32, 32) for x in image_patches]
+
     predictions = model.predict(np.asarray(image_patches), batch_size=32, verbose=verbose)[:, 0, :, :, :]
     predicted_image = recreate_image_from_patches(original_image_size=original_image_size, list_patches=predictions)
     return predicted_image
