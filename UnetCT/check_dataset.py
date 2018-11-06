Fixed normalisation : now handles min/max scaling

UnetCT/check_dataset.py
@@ -44,7 +44,7 @@ if __name__ == '__main__':
                 max_img = np.max(img)
                 min_img = np.min(img)
                 if max_img>1 or min_img<-1:
-                    dict_log[file] = (min_img,max_img)
+                    dict_log[file] = (min_img, max_img)
 
                 if c == "lesion":
                 #Log number of files with lesion

UnetCT/image_processing.py
@@ -12,9 +12,13 @@ def preprocess_image(img, preprocessing="standardize"):
         return standardize(img)
 
 def standardize(img):
-    img_norm = normalize_numpy(img)
+    # scale between 0 and 1
+    img_norm = normalize_numpy(img, new_min=0.0, new_max=1.0)
+    # set mean = 0 and std = 1
     img_new = (img_norm-np.mean(img_norm))/np.std(img_norm)
-    return img_new
+    # scaling between -1 and 1
+    img_new_norm = normalize_numpy(img_new, new_min=-1.0, new_max=1.0)
+    return img_new_norm
 
 def load_data_atlas_for_patient(patient_path):
     patient = os.path.basename(patient_path)
@@ -192,7 +196,6 @@ def to_patches_3d(toPatch,PATCH_X,STRIDE_PATCH_X,PATCH_Y,STRIDE_PATCH_Y,PATCH_Z,
                 patches.append(cut)
 
     patches = np.array(patches)
-    print(patches.shape)
     return patches
 
 

UnetCT/utils.py
@@ -4,12 +4,12 @@ from sklearn.model_selection import train_test_split
 from shutil import copyfile
 import numpy as np
 
-def normalize_numpy(v, max_value=1.0, min_value=0.0):
-    m = v.max()
-    if m==0:
-        return np.zeros(v.shape)
-    else:
-        return v*max_value/m
+def normalize_numpy(v, new_max=1.0, new_min=0.0):
+    min_v = np.min(v)
+    max_v = np.max(v)
+
+    v_new = (v-min_v)*(new_max-new_min)/(max_v-min_v) + new_min
+    return v_new
 
 
 def create_if_not_exists(directory):