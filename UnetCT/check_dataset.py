32ee6255a5d1b82831db4829cf8d67b216608193
Simon Narduzzi <simon.narduzzi@gmail.com>
2018-11-17 17:16:58 +0100

    Printing statistics about voxels

--- UnetCT/check_dataset.py
+++ UnetCT/check_dataset.py
@@ -32,6 +32,10 @@ if __name__ == '__main__':
         total_not_containing_lesion = 0
         total_lesion_files = 0
 
+        total_lesion_voxels = 0
+        total_not_lesion_voxels= 0
+        total_voxels = 0
+
         for c in channels:
             subsubdir = os.path.join(subdirectory, c)
             files = os.listdir(subsubdir)
@@ -55,9 +59,18 @@ if __name__ == '__main__':
                         total_not_containing_lesion+=1
                     total_lesion_files+=1
 
+                    total_lesion_voxels += np.sum(img)
+                    total_voxels_img = img.shape[0]*img.shape[1]*img.shape[2]
+                    total_not_lesion_voxels += (total_voxels_img-np.sum(img))
+                    total_voxels += total_voxels_img
+
+
         dict_log["{}_CONTAINING_LESION".format(f)] = total_containing_lesion
         dict_log["{}_NOT_CONTAINING_LESION".format(f)] = total_not_containing_lesion
         dict_log["{}_TOTAL_FILES".format(f)] = total_lesion_files
+        dict_log["{}_VOXELS_CONTAINING_LESION".format(f)] = total_lesion_voxels
+        dict_log["{}_VOXELS_NOT_CONTAINING_LESION".format(f)] = total_not_lesion_voxels
+        dict_log["{}TOTAL_VOXELS".format(f)] = total_voxels
 
     # Save report
     json_file = os.path.join(directory, "report.json")
@@ -74,12 +87,22 @@ if __name__ == '__main__':
         total_lesion = dict_log["{}_CONTAINING_LESION".format(f)]
         total_not_lesion = dict_log["{}_NOT_CONTAINING_LESION".format(f)]
         total = dict_log["{}_TOTAL_FILES".format(f)]
+        total_lesion_voxels =  dict_log["{}_VOXELS_CONTAINING_LESION".format(f)]
+        total_not_lesion_voxels =  dict_log["{}_VOXELS_NOT_CONTAINING_LESION".format(f)]
+        total_voxels = dict_log["{}TOTAL_VOXELS".format(f)]
 
         print("--- {} ".format(f.upper()))
-        print("Lesions : {} ({}%)".format(str(total_lesion), str(100*total_lesion/total)))
-        print("Not lesions : {} ({}%)".format(str(total_not_lesion), str(100*total_not_lesion / total)))
-        print("TOTAL : {} ({}% of total dataset)".format(str(total),str(100*total/total_dataset_size)))
+        print("Lesions files: {} ({}%)".format(str(total_lesion), str(100*total_lesion/total)))
+        print("Not lesions files: {} ({}%)".format(str(total_not_lesion), str(100*total_not_lesion / total)))
+        print("TOTAL : {} ({}% of total dataset)".format(str(total), str(100*total/total_dataset_size)))
         print("")
+
+        print("--- {} ".format(f.upper()))
+        print("Lesions voxels: {} ({}%)".format(str(total_lesion_voxels), str(100 * total_lesion_voxels / total_voxels)))
+        print("Not lesions voxels: {} ({}%)".format(str(total_not_lesion_voxels), str(100 * total_not_lesion_voxels / total_voxels)))
+        print("Total voxels : {}").format(str(total_voxels))
+        print("")
+
 
     print("Total dataset size : {} files.".format(total_dataset_size))
 
